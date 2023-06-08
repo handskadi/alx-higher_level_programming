4a53d42cb98bfa7ad013efffdd48220e3c6bc7b8
Mohamed KADI <handskadi@gmail.com>
2023-06-08 23:47:08 +0000

    task 2: init

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        print("{:d} argument:".format(len(sys.argv)-1))
+        print("1: {:s}".format(sys.argv[1]))
+    elif len(sys.argv) > 2:
+         print("{:d} arguments:".format(len(sys.argv)-1))
+         i = 1
+         while i < len(sys.argv):
+             print("{:d}: {:s}".format(i, sys.argv[i]))
+             i+=1
+    else:
+        print("0 arguments.")
+

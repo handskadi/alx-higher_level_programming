7975c06c86c7373ec59c0fef60c6c138e47e9d03
Mohamed KADI <handskadi@gmail.com>
2023-06-08 23:58:58 +0000

    "tast2: fix pycode er

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,11 +6,10 @@ if __name__ == "__main__":
         print("{:d} argument:".format(len(sys.argv)-1))
         print("1: {:s}".format(sys.argv[1]))
     elif len(sys.argv) > 2:
-         print("{:d} arguments:".format(len(sys.argv)-1))
-         i = 1
-         while i < len(sys.argv):
-             print("{:d}: {:s}".format(i, sys.argv[i]))
-             i+=1
-    else:
-        print("{:d} argument.".format(0))
-
+        print("{:d} arguments:".format(len(sys.argv)-1))
+        i = 1
+        while i < len(sys.argv):
+            print("{:d}: {:s}".format(i, sys.argv[i]))
+            i += 1
+        else:
+            print("{:d} argument.".format(0))

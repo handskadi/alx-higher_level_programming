7a5b28a6f0534313f2f44884a20f88fee19b875e
Mohamed KADI <handskadi@gmail.com>
2023-06-09 00:10:11 +0000

    Tsk 2: final fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-import sys
-
 if __name__ == "__main__":
+    import sys
     if len(sys.argv) == 2:
         print("{:d} argument:".format(len(sys.argv)-1))
         print("1: {:s}".format(sys.argv[1]))

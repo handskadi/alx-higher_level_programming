8bfd76057257ea43bbe1f2fd47fcf34834109c5b
Mohamed KADI <handskadi@gmail.com>
2023-06-10 09:00:49 +0000

    Task 4: Pycodestyle fix

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    #Alter element in copy list
+    # Alter element in copy list
     if idx < 0 or idx > (len(my_list) - 1):
         return (my_list)
     cy = [x for x in my_list]

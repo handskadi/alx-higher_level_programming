b22e85bf9ef7f5587700c07df4d7db10f8384667
Mohamed KADI <handskadi@gmail.com>
2023-06-10 09:12:41 +0000

    Fix confusion of vars and list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,6 +3,9 @@ def max_integer(my_list=[]):
     # Get the biggest elment in the list
     if len(my_list) < 1:
         return None
-    list_cy = my_list.cy()
+    list_cy = my_list
     list_cy.sort()
     return list_cy[-1]
+
+# my_list = [1,2,5,45,6]
+# print(max_integer(my_list))

6a52d656a710d28baf9b8a1ec436f41dbe401f5b
Mohamed KADI <handskadi@gmail.com>
2023-06-14 16:47:12 +0000

    Task 11: fix

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-
-# Define a func that returns a list with all values * num (no loop)
-
-
-def multiply_list_map(my_list=[], number=0):
-    return list(map(lambda x: x * number, my_list))
+def mutiply_list_map(my_list=[], number=0):
+    return (list(map(lambda x: x * number, my_list)))

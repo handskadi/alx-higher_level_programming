b624d3c99173cb216771f02ff1804e1a7fb6e11f
Mohamed KADI <handskadi@gmail.com>
2023-06-26 15:15:26 +0000

    task: 3 Revision

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     tracker = 0
-    for i in range(x):
+    for idx in range(x):
         try:
-            print("{:d}".format(my_list[i]), end="")
+            print("{:d}".format(my_list[idx]), end="")
             tracker = tracker + 1
         except (TypeError, ValueError):
             pass

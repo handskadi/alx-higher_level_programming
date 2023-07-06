985e5404683aa030b7709c412e03861096d34597
Mohamed KADI <handskadi@gmail.com>
2023-07-06 22:55:53 +0000

    TTD: task 0 init`~:

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Add_integer method Module"""
+
+
+def add_integer(a, b=98):
+    """ Two integers addtion."""
+    if type(a) not in (int, float):
+        raise TypeError("a must be an integer")
+    elif type(b) not in (int, float):
+        raise TypeError("b must be an integer")
+    else:
+        return int(a) + int(b)
+
+    if __name__ == "__main__":
+        import doctest
+        doctest.testfile("tests/0-add_integer.txt")

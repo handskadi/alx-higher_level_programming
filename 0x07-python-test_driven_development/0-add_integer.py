1588d371978078290f3057c1491c0d6b73d7f26e
Mohamed KADI <handskadi@gmail.com>
2023-07-06 23:17:02 +0000

    TTD Tasks: init 2

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
-"""
-Add_integer method Module
-"""
+""" Integer addition function."""
 
 
 def add_integer(a, b=98):
-    """ Two integers addtion."""
-    if type(a) not in (int, float):
-        raise TypeError("a must be an integer")
-    elif type(b) not in (int, float):
-        raise TypeError("b must be an integer")
-    else:
-        return int(a) + int(b)
+    """Return int add of a and b.
 
-    if __name__ == "__main__":
-        import doctest
-        doctest.testfile("tests/0-add_integer.txt")
+    Float arguments are typecsted.
+
+    Raises:
+        TypeError: If either of a or b is a non-integer and non-float.
+    """
+    if ((not isinstance(a, int) and not isinstance(a, float))):
+        raise TypeError("a must be an integer")
+    if ((not isinstance(b, int) and not isinstance(b, float))):
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))

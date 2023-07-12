14b9db576461d20fe61133c90cecc86070e71dc3
Mohamed KADI <handskadi@gmail.com>
2023-07-12 11:25:21 +0000

    Task 8: init

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Rectangle Mod """
+
+
+class Rectangle(BaseGeometry):
+    """ Rectangle CLass """
+    def __init__(self, width, height):
+        """ initiate rect method """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height

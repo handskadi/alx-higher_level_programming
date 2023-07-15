b841c5ea2b1c9e29ffdcf74c2102e73545278190
Mohamed KADI <handskadi@gmail.com>
2023-07-15 09:02:46 +0000

    Task 1: init

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -6,6 +6,11 @@ class Base:
     """ Base Class """
     __nb_objects = 0
     def __init__(self, id=None):
-        self.id = id
+        """ Constractor """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
 
 

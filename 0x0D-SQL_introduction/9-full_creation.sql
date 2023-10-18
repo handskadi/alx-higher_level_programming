12e5838259cecaf854d91627c8804d2237e4ec85
Mohamed KADI <handskadi@gmail.com>
2023-10-18 01:06:51 +0000

    Task 9 fix commas

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,7 +1,7 @@
 -- A scripts that do full crewation of a table
 CREATE TABLE IF NOT EXISTS second_table (
-	id INT
-	name VARCHAR(256)
+	id INT,
+	name VARCHAR(256),
 	score INT
 );
 INSERT INTO second_table (id, name, score) VALUES (1, "John", 10);

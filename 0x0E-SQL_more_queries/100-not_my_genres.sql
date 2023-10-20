9efb56cc523b378b3b15c0827b56059cf6108e59
Mohamed KADI <handskadi@gmail.com>
2023-10-20 00:26:06 +0000

    Taskk 100 fixed

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,9 +1,11 @@
 -- list all genres not linked to the show "Dexter"
-SELECT `name` FROM `tv_genres`
-WHERE `name` NOT IN (
-	SELECT `name` FROM `tv_shows`
-	INNER JOIN `tv_show_genres` ON `tv_shows`.`id` = `tv_show_genres`.`show_id`
-	INNER JOIN `tv_genres` ON `tv_shows_genre`.`genre_id` = `tv_genres`.`id`
-	WHERE `tv_shows`.`title` = "Dexter"
-)
-ORDER BY `name`;
+SELECT name
+FROM tv_genres
+WHERE name NOT IN
+(SELECT name
+	FROM tv_genres
+	LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+	LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+	WHERE tv_shows.title = 'Dexter')
+GROUP BY name
+ORDER BY name ASC;

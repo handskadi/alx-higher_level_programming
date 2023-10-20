31b383725309921d87258fe58da41ad62df3e78c
Mohamed KADI <handskadi@gmail.com>
2023-10-20 00:27:40 +0000

    Task 101 fixed

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,8 +1,11 @@
 -- list all shows without the Comedy
-SELECT `title` FROM `tv_shows`
-WHERE `title` NOT IN (
-	SELECT `title` from `tv_genres`
-	INNER JOIN `tv_show_genres` on `tv_show_genres`.`genre_id` = `tv_genres`.`id`
-	RIGHT JOIN `tv_shows` on `tv_genres`.`show_id` = `tv_shows`.`id`
-)
-ORDER BY `title`;
+SELECT title
+FROM tv_shows
+WHERE title NOT IN
+(SELECT title
+	FROM tv_shows
+	LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+	LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+	WHERE tv_genres.name = 'Comedy')
+GROUP BY title
+ORDER BY title ASC;i

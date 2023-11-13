511538827dac30731c2171eb0f596403f7fe6e58
Mohamed KADI <handskadi@gmail.com>
2023-11-13 15:47:45 +0000

    task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,8 @@
-#!/usr/bin/node
+#!/usr/bin/nod
 const argc = process.argv.length;
 
 if (argc > 2) {
-  console.log('Argument' + (argc > 3 ? 's' : '') + ' found');
+	console.log('Argument' + (argc > 3 ? 's' : '') + ' found');
 } else {
-  console.log('No argument');}
+	console.log('No argument');
+}

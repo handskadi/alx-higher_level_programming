5338a21514bf4808135a7f9fcb83e7adad150522
Mohamed KADI <handskadi@gmail.com>
2023-11-13 15:56:49 +0000

    Task 6 processed

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const myVar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const langs = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 
-myVar.forEach((element) => {
-	console.log(element);
-});
+for (let i = 0; i < 3; i++) {
+	console.log(langs[i]);
+}

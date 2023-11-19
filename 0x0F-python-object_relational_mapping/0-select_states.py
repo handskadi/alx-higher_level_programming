8daaa76bd89e1968e99f66432c0038177485d69c
Mohamed KADI <handskadi@gmail.com>
2023-11-19 20:10:55 +0000

    Task 0 init

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+This script lists all states from the
+database `hbtn_0e_0_usa`.
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    """
+    Access to the database and get the states
+    from the database.
+    """
+
+    # Connecting to Database
+    conn = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3])
+
+    # Selecting Data using cursor
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+
+    # Print Data
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+
+    # Close connection
+    cur.close()
+    conn.close()

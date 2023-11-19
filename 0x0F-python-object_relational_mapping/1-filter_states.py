5997edf0a0aac7fd5476f9a3481bc9f7d538dd6b
Mohamed KADI <handskadi@gmail.com>
2023-11-19 20:53:09 +0000

    Task 2 fix 4

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -18,7 +18,6 @@ if __name__ == '__main__':
         "SELECT * FROM states WHERE name LIKE BINARY 'N%' \
                 ORDER BY states.id ASC")
 
-
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)

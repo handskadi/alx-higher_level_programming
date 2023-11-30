3ae081d739a6e23ce4449a482cd827ef1f73c4bf
Mohamed KADI <handskadi@gmail.com>
2023-11-30 15:53:04 +0000

    Task 05: Fix

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -9,23 +9,16 @@ def find_peak(list_of_integers):
         list_of_integers(int): list 
     Returns: peak of list
     """
-    size = len(list_of_integers)
-    mid_e = size
-    mid = size // 2
-
-    if size == 0:
+    if not list_of_integers:
         return None
+    low, high = 0, len(list_of_integers) - 1
 
-    while True:
-        mid_e = mid_e // 2
-        if (mid < size - 1 and
-                list_of_integers[mid] < list_of_integers[mid + 1]):
-            if mid_e // 2 == 0:
-                mid_e = 2
-            mid = mid + mid_e // 2
-        elif mid_e > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
-            if mid_e // 2 == 0:
-                mid_e = 2
-            mid = mid - mid_e // 2
+    while low < high:
+        mid = (low + high) // 2
+
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            high = mid
         else:
-            return list_of_integers[mid]
+            low = mid + 1
+    return list_of_integers[low]
+

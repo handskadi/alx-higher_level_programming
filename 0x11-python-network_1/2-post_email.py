a84f324c7603afccbf48d2ed6af7d1d644ac3861
Mohamed KADI <handskadi@gmail.com>
2023-12-01 14:48:36 +0000

    Task 2: valid

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,9 +7,9 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    val = {"email": sys.argv[2]}
+    email = {"email": sys.argv[2]}
 
-    data = urllib.parse.urlencode(val).encode("ascii")
+    data = urllib.parse.urlencode(email).encode("ascii")
     req = urllib.request.Request(url, data)
 
     with urllib.request.urlopen(req) as res:

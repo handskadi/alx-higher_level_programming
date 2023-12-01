d168dcd5571b403ca66032f5b378fe013f9c2c6b
Mohamed KADI <handskadi@gmail.com>
2023-12-01 14:16:10 +0000

    Task 1: init

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     url = sys.argv[1]
 
     with urllib.request.urlopen(url) as res:
-
-        x_req_id = res.getheader('X-Request-Id')
+        x_req_id = dict(res.headers).get("X-Request-Id")
         print(x_req_id)
+        

78cf2aa0d0ab18d7948d608309c896be886c4be5
Mohamed KADI <handskadi@gmail.com>
2024-01-03 11:13:57 +0000

    Task 100: init

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -7,20 +7,20 @@ const url = `https://swapi.dev/api/films/${movieId}/`;
 
 request.get(url, (error, response, body) => {
   if (error) {
-    console.log(error);
+    console.error(error);
     return;
   }
 
   const data = JSON.parse(body);
   const characters = data.characters;
-  for (const character of characters) {
-    request(character, (error, response, body) => {
+  characters.forEach((characterUrl) => {
+    request.get(characterUrl, (error, response, body) => {
       if (error) {
-        console.log(error);
+        console.error(error);
         return;
       }
       const characterData = JSON.parse(body);
       console.log(characterData.name);
     });
-  }
+  });
 });
